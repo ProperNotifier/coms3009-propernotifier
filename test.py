aab1a1cfd04a9c409408d2855677b22e376bdbb3
ProperNotifier <30655392+ProperNotifier@users.noreply.github.com>
2017-08-23 21:54:54 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,5 +2,9 @@ print("Hello World")
 
 for i in range(10):
   print("Goodbye, cruel world")
-if 1==2:
-  print("that's not right")
+
+try:
+    mode=int(raw_input('Input:'))
+    print mode
+except ValueError:
+    print "Not a number"
