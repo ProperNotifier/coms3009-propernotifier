aec3d200b31fd4ae18f85204bf36d9e34cac2e8a
1076589 <1076589@students.wits.ac.za>
2017-09-04 14:13:05 +0200

    gitignore added, max fun defined

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
 	public static void main(String[]args) {
 		System.out.println("Hello World");
+		System.out.println(max(5,6));
+	}
+	public static int max(int a, int b) {
+		return (a > b? a:b);
 	}
 }
