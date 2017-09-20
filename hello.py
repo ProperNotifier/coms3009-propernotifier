da07d555d6558edee22208f3149da3e19d3780ff
1076589 <1076589@students.wits.ac.za>
2017-09-20 18:41:03 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 def read_stdin(number):
-    return 5
-#    try:
-#        mode=int(number) #raw_input('Input:'))
-#        return mode
-#    except ValueError:
-#	return "Not a number"
+    if 1 > 2:
+        return 5
+    try:
+        mode=int(number) #raw_input('Input:'))
+        return mode
+    except ValueError:
+	return "Not a number"
