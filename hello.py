5fb8eef0b9e01d046cffda699078d9a7e3cb647a
1076589 <1076589@students.wits.ac.za>
2017-09-20 18:19:03 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 print("Hello World")
 
 for i in range(10):
-  print("Goodbye, cruel world")
+	print("Goodbye, cruel world")
 
 def read_stdin(number):
 	try:
 		mode=int(number) #raw_input('Input:'))
 		return mode
 	except ValueError:
-return "Not a number"
+	return "Not a number"
