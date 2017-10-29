810eba1452e2a7b853e5013146a7dfa7b2813c70
Tau Merand <tau.merand@students.wits.ac.za>
2017-10-29 20:43:29 +0200

    Trying to increase coverage

--- test_classifier.py
+++ test_classifier.py
@@ -1,5 +1,17 @@
 from nose.tools import *
+from skimage import io
+
 from recognition import *
+
+
+def _json_to_bboxes(file):
+    bboxes = []
+    json_dicts = json.loads(file)
+    for curr_dict in json_dicts:
+        bbox = BoundingBox(curr_dict["left"], curr_dict["right"], curr_dict["top"], curr_dict["bottom"],
+                           curr_dict["label"])
+        bboxes.append(bbox)
+    return bboxes
 
 
 class TestClassifier:
@@ -14,3 +26,9 @@ class TestClassifier:
         new_img = self.classifier._resize(img)
         eq_(new_img.shape, (45, 45))
 
+    def test_recognition(self):
+        img = io.imread("test_data/APPM200715.jpg")
+        with open("test_data/APPM200715.json", 'r') as f:
+            json_string = json.load(f)
+        bboxes = _json_to_bboxes(json_string)
+        assert self.classifier.classify(bboxes, img)
